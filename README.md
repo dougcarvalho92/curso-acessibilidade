31d77fc39af41e0fe684e341cfe70ab922a3f41c
Douglas Carvalho <dscarvalho199@gmail.com>
2025-10-22 13:53:58 -0300

    feat: Update README and add accessibility tools; refactor page structure and styles

--- README.md
+++ README.md
@@ -1,36 +1,20 @@
-This is a [Next.js](https://nextjs.org) project bootstrapped with [`create-next-app`](https://nextjs.org/docs/app/api-reference/cli/create-next-app).
+# Extensões e Ferramentas para Acessibilidade
 
-## Getting Started
+### AXE Core
+Uma biblioteca de testes automatizados para acessibilidade web, especialmente integrada com React.
+- Detecta problemas de acessibilidade durante o desenvolvimento
+- Fornece sugestões de correção
+- Link: https://www.npmjs.com/package/@axe-core/react
 
-First, run the development server:
+### ESLint-plugin-jsx-a11y
+Plugin ESLint para identificar e corrigir problemas de acessibilidade em código JSX.
+- Verifica padrões de acessibilidade no código
+- Integra com o workflow de desenvolvimento
+- Link: https://www.npmjs.com/package/eslint-plugin-jsx-a11y
 
-```bash
-npm run dev
-# or
-yarn dev
-# or
-pnpm dev
-# or
-bun dev
-```
-
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
-
-You can start editing the page by modifying `app/page.tsx`. The page auto-updates as you edit the file.
-
-This project uses [`next/font`](https://nextjs.org/docs/app/building-your-application/optimizing/fonts) to automatically optimize and load [Geist](https://vercel.com/font), a new font family for Vercel.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/app/building-your-application/deploying) for more details.
+### Landmark Navigation
+Extensão Chrome para visualização e navegação de landmarks ARIA.
+- Ajuda a identificar regiões importantes da página
+- Permite navegação rápida via atalhos de teclado
+- Útil para testar a estrutura de landmarks da página
+- Link: https://chromewebstore.google.com/detail/landmark-navigation-via-k/ddpokpbjopmeeiiolheejjpkonlkklgp
--- app/page.tsx
+++ app/page.tsx
@@ -11,27 +11,48 @@ import styles from "./styles/home.module.css";
 export default function Home() {
   useEffect(() => {
     axeAccessibilityReporter();
-},[])
+  }, []);
   return (
-    <div>
-      <div className={styles.navbar}>
+    <>
+      <header className={styles.navbar}>
         <Image src={Logo} width={50} />
-        <div className="nav">
-          <Link href="github.com/dougcarvalho92/"><Github size={25}/></Link>
+        <div className='nav'>
+          <Link href='github.com/dougcarvalho92/'>
+            <Github size={25} />
+          </Link>
         </div>
-      </div>
+      </header>
       <main className={styles.container}>
-        <h1>Welcome to Next.js!</h1>
-        <p>
-          Get started by editing&nbsp;
-          <code>app/page.tsx</code>
-        </p>
-        <h4>TESTE</h4>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi temporibus tempora similique laboriosam eveniet, cum quae cumque. Iste ad sequi similique. Quibusdam suscipit quod facilis modi mollitia, nostrum quam odio!</p>
-        <h3>H3 TESTE</h3>
-        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ab repellendus architecto veritatis obcaecati voluptatibus dicta accusantium totam optio ad, sint iste aliquam sit tenetur eaque accusamus consectetur magnam recusandae.</p>
+        <article>
+          <h1>Welcome to Next.js!</h1>
+          <p>
+            Get started by editing&nbsp;
+            <code>app/page.tsx</code>
+          </p>
+          <h4>TESTE</h4>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nisi
+            temporibus tempora similique laboriosam eveniet, cum quae cumque.
+            Iste ad sequi similique. Quibusdam suscipit quod facilis modi
+            mollitia, nostrum quam odio!
+          </p>
+          <h3>H3 TESTE</h3>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse ab
+            repellendus architecto veritatis obcaecati voluptatibus dicta
+            accusantium totam optio ad, sint iste aliquam sit tenetur eaque
+            accusamus consectetur magnam recusandae.
+          </p>
+        </article>
       </main>
-      
-    </div>
+      <footer className={styles.navbar} aria-label='Rodapé'>
+        <Image src={Logo} width={50} />
+        <div className='nav'>
+          <Link href='github.com/dougcarvalho92/'>
+            <Github size={25} />
+          </Link>
+        </div>
+      </footer>
+    </>
   );
 }
